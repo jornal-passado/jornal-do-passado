cb2612883c809f35bfe9d7b9f06a66ec3ae32395
Bruno Galhardo <bgalhardo@gmail.com>
2020-04-04 15:41:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/throwback/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.throwback;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void startGame(View view) {
+        // Do something in response to button
+    }
+}
